58add5bae8735c9bc36e4a662d2a81a57a1bd064
GlebLobinsky <Lobinsky.gleb@gmail.com>
2021-07-19 17:19:59 +0300

    fixies

--- src/settings.py
+++ src/settings.py
@@ -81,9 +81,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get("NAME"),
         'USER': os.environ.get("USER"),
-        "PASSWORD": os.environ.get('PASSWORD'),
+        'PASSWORD': os.environ.get('PASSWORD'),
         'HOST': os.environ.get('HOST'),
-        'PORT': 5432
+        'PORT': 5432,
     }
 }
 

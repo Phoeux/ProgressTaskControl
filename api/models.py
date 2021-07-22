23df93c33d8075e12cf1a78c1e8b15443da3bb7e
GlebLobinsky <Lobinsky.gleb@gmail.com>
2021-07-22 12:37:03 +0300

    fixes

--- api/models.py
+++ api/models.py
@@ -22,7 +22,7 @@ class Tasks(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     links = models.ManyToManyField('Links')
-    progress = models.CharField(max_length=20)
+    progress = models.CharField(max_length=20, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='simple_user')
     manager = models.ForeignKey(User, on_delete=models.CASCADE, related_name='manager_user')
     finished = models.BooleanField(default=False)
